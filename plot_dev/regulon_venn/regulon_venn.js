9b5bd16564302e8356b9c562aa3b273b4ffd89c9
Kevin Rychel <krychel@eng.ucsd.edu>
2020-05-05 13:07:26 -0700

    Plot Dev File Structure, Histograms
    Reworked entire site to facilitate collaborative plotting. Added template files for all plots in original dashboard. Created gene histograms.

---
+++ plot_dev/regulon_venn/regulon_venn.js
@@ -0,0 +1,11 @@
+/**
+ * @summary Creates interactive gene weight histograms for ModulomeVis
+ * @author INSERT NAME
+ * requires INSERT REQUIRED CDNs
+ */
+ 
+ // edit function below
+ // Write Highcharts plot to container (see gene_histogram.js for example)
+ function generateGeneTable(csvContent, container) {
+    
+ };
