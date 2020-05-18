34ccc852b5da5eef21c575691081f0f52bf7bf68
Katherine Decker <54454225+KatherineDecker@users.noreply.github.com>
2020-05-18 00:03:21 -0700

    regulon_venn.py: Added function to get dataframes of gene counts for data input into venns

--- plot_dev/regulon_venn/regulon_venn.py
+++ plot_dev/regulon_venn/regulon_venn.py
@@ -1,21 +1,30 @@
-# update doc string and delete this line
-""" gene_table.py: 
-    Generates .csv files to be read by gene_table.js for ModulomeVis site
+""" regulon_venn.py:
+    Generates .csv files to be read by regulon_venn.js for ModulomeVis site
 """
 
-import numpy as np
-import pandas as pd
+def gene_hist_df(ica_data, k):
+    tf = enrich.TF[k]
 
-# insert helper functions if necessary
+    # Take care of and/or enrichments
+    if '+' in tf:
+        reg_list = []
+        for tfx in tf.split('+'):
+            reg_list.append(set(ica_data.trn[ica_data.trn.TF == tfx].gene_id.unique()))
+        reg_genes = set.intersection(*reg_list)
+    elif '/' in tf:
+        reg_genes = set(ica_data.trn[ica_data.trn.TF.isin(tf.split('/'))].gene_id.unique())
+    else:
+        reg_genes = set(ica_data.trn[ica_data.trn.TF == tf].gene_id.unique())
 
-# edit function name, inputs, doc string, etc...
-def gene_table_df(ica_data, k, row):
-    '''
-    input: ica_data, from github.com/SBRG/ICA
-           k, the i-modulon's index
-           row, its associated row in curated_enrichments.csv
-    output: a dataframe for producing the table (in this case, just the table)
-    '''
-    res = pd.DataFrame()
-    
-    return res
+    # Get component genes and operons
+    comp_genes = set(ica_data.show_enriched(k).index)
+    both_genes = set(reg_genes & comp_genes)
+
+    reg_operons = len(ica_data.genes2operons(reg_genes))
+    comp_operons = len(ica_data.genes2operons(comp_genes))
+    both_operons = len(ica_data.genes2operons(reg_genes & comp_genes))
+
+    return (pd.DataFrame([len(reg_genes), len(comp_genes), len(both_genes),
+                          reg_operons, comp_operons, both_operons], columns=['Number'],
+                         index=['reg_genes', 'comp_genes', 'both_genes',
+                                'reg_ops', 'comp_ops', 'both_ops']))
